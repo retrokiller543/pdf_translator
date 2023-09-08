a4fef3bb97c71a4b7b810b420f7e0928c0a33c86
Retrokiller543 <emil.schutt@gmail.com>
2023-09-08 11:50:09 +0200

    other: removed some prints in script

--- increment_version.sh
+++ increment_version.sh
@@ -3,19 +3,12 @@ git fetch --tags
 tag=$(git describe --tags --abbrev=0)
 
 major=$(echo $tag | cut -d. -f1 | cut -c2-)
-echo "major = $major"
 minor=$(echo $tag | cut -d. -f2)
-echo "minor = $minor"
 patch=$(echo $tag | cut -d. -f3)
-echo "patch = $patch"
 
 messages=$(git log $tag..HEAD --pretty=format:%s)
 
 versioning_commits=$(echo "$messages" | grep -E "^(feat|major|fix|minor|chore|refactor|patch):")
-
-echo $(git log --oneline -n 5)
-echo $(git tag)
-
 
 if [ -z "$versioning_commits" ]; then
   echo "No versioning commits detected. No version increment needed."
@@ -28,27 +21,19 @@ patch_count=$(echo "$messages" | grep -cE "^(chore|refactor|patch):")
 
 if [[ $major_count -ge $minor_count ]] && [[ $major_count -ge $patch_count ]]; then
   # Increment the major number
-  echo "Incrementing major version"
   major=$((major + 1))
   minor=0
   patch=0
-  echo "New version is $major.$minor.$patch"
 elif [[ $minor_count -ge $major_count ]] && [[ $minor_count -ge $patch_count ]]; then
   # Increment the minor number
-  echo "Incrementing minor version"
   minor=$((minor + 1))
   patch=0
-  echo "New version is $major.$minor.$patch"
 else
   # Increment the patch number
-  echo "Incrementing patch version"
   patch=$((patch + 1))
-  echo "New version is $major.$minor.$patch"
 fi
 
 version="$major.$minor.$patch"
-echo "Current version is $tag"
-echo "New version is $version"
 
 # Update the version in Cargo.toml
 echo "Updating version to $version"
